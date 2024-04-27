bd47293a7b243acbba25d43fcfaabaec609029d9
frank-mosigisi <frankmosigisi427@gmail.com>
2024-04-27 05:12:44 -0500

    added generateNewGrid function and refactored applyHoverEffect

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const gridcontainer = document.querySelector('#container')
 const clearButton = document.querySelector('.clearbutton')
+const newGameButton = document.querySelector('.newGamebutton')
+
+let defaultSquares = 16;
 
 function fullGrid (gridCount){
 
@@ -18,21 +21,65 @@ function fullGrid (gridCount){
     }
 }
 
-fullGrid(16);
+fullGrid(defaultSquares);
+applyHoverEffect()
 
-const squares = document.querySelectorAll('.square');
-
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', (e) => {
-        e.target.classList.add('active')
-    });
-});
-
-clearButton.addEventListener('click', ()=>{
+function applyHoverEffect() {
+    const squares = document.querySelectorAll('.square');   
     squares.forEach((square) => {
-            square.classList.remove('active')
+        square.addEventListener('mouseenter', (e) => {
+            e.target.classList.add('active')
         });
+    });
+
+    // added the clear button event listener too inside the
+    // function so that it can also be reapplied when a new grid 
+    // is created too
+    clearButton.addEventListener('click', ()=>{
+        squares.forEach((square) => {
+                square.classList.remove('active')
+            });
     })
+}
+
+
+
+
+
+
+function generateNewGrid() {
+
+    let number;
+
+    while(true){
+
+        number = prompt ('Hello player one, enter the number of squares per side for the new grid'
+                           + ' Enter a number less than 101.' , '')
+        if (number=== null || number === ''){
+            number = defaultSquares;
+            break;
+        }
+
+        number = Number(number);
+        if (Number.isNaN(number)){
+            alert ('please enter a valid number!');
+        }
+        else if (number > 100 || number < 1){
+            alert('You entered a number over 101 or less than 1! Try again. Thanks');
+
+        }else{
+            break;
+        }
+    }
+    while (gridcontainer.firstChild) {
+        gridcontainer.removeChild(gridcontainer.firstChild);
+    
+    }
+    fullGrid(number)
+    applyHoverEffect()
+
+}
+newGameButton.addEventListener('click', generateNewGrid);
     
 
 
